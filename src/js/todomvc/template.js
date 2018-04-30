c78f1147bcbc3a3b83d07591ee3bad6b7c4f7a39
Dongwoo Lee <likedemian@me.com>
2018-05-01 00:06:19 +0900

    add: todo features

---
+++ src/js/todomvc/template.js
@@ -0,0 +1,21 @@
+import {
+  ItemList
+} from './item';
+
+import {
+  escapeForHTML
+} from './helpers';
+
+export default class Template {
+  itemList(items) {
+    return items.reduce((a, item) => a + `
+<li data-id="${item.id}"${item.completed ? ' class="completed"' : ''}>
+	<input class="toggle" type="checkbox" ${item.completed ? 'checked' : ''}>
+	<label>${escapeForHTML(item.title)}</label>
+	<button class="destroy"></button>
+</li>`, '');
+  }
+  itemCounter(activeTodos) {
+    return `${activeTodos} item${activeTodos !== 1 ? 's' : ''} left`;
+  }
+}
